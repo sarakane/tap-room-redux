09f9e436f1973b63705682b775d18d4746fd86d8
Sara Kane <kane.sarak@gmail.com>
2020-09-18 15:44:04 -0700

    Add logic to make test 'Should successfully add selected keg data to selectedKeg' pass for 'selected-keg-reducer

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,5 +1,18 @@
 import * as c from './../actions/ActionTypes';
 
 export default (state = null, action) => {
-  return state;
+  const {name, brand, price, alcoholContent, pints, id} = action;
+  switch (action.type) {
+    case c.SELECT_KEG:
+      return  {
+        name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          pints: pints,
+          id: id
+      };
+    default:
+      return state;
+  }
 };
