6262b96e3def7019f4c32109b14410e5eecc48f9
Sara Kane <kane.sarak@gmail.com>
2020-09-18 09:40:48 -0700

    Add failing test 'Should successfully add new keg data to masterKegList' for keg-list-reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: "Name",
+    brand: "BRAND",
+    price: 7.5,
+    alcoholContent: 5.5,
+    pints: 124,
+    id: 1
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+  
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, alcoholContent, pints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pints: pints,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pints: pints,
+        id: id
+      }
+    })
+  })
 });
