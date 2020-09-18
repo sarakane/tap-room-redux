ce5dd31fcac59dd16c432efd90098ac3c529bf72
Sara Kane <kane.sarak@gmail.com>
2020-09-18 14:10:51 -0700

    Add action tests for connected reducers in index reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore} from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
+import * as a from '../../actions/index';
 
 let store = createStore(rootReducer);
 
@@ -19,5 +20,28 @@ describe('rootReducer', () => {
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
-  })
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    const keg = {
+      type: 'ADD_KEG',
+      name: 'Name',
+      brand: 'BRAND',
+      price: 7.5,
+      alcoholContent: 5.5,
+      pints: 124,
+      id: 1
+    };
+    const action = a.addKeg(keg);
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
