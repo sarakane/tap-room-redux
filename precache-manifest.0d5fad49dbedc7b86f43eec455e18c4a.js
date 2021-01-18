47c69f9e81ed814e652031c5604ff55155367ff2
Sara Kane <7584520+sarakane@users.noreply.github.com>
2021-01-18 12:09:15 -0800

    Updates

---
+++ precache-manifest.0d5fad49dbedc7b86f43eec455e18c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2e1efec8c22b652ad0e808df3372d2",
+    "url": "/tap-room-redux/index.html"
+  },
+  {
+    "revision": "893b8911e599fb7f2c39",
+    "url": "/tap-room-redux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "95542ac1ba468f2583cb",
+    "url": "/tap-room-redux/static/css/main.1a8f37e3.chunk.css"
+  },
+  {
+    "revision": "893b8911e599fb7f2c39",
+    "url": "/tap-room-redux/static/js/2.a11699ee.chunk.js"
+  },
+  {
+    "revision": "95542ac1ba468f2583cb",
+    "url": "/tap-room-redux/static/js/main.58b0534f.chunk.js"
+  },
+  {
+    "revision": "1a8559203586c1fbf73a",
+    "url": "/tap-room-redux/static/js/runtime-main.03f28298.js"
+  }
+]);
